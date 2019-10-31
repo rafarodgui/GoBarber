5c89ccea987678a3089e9b8a194e55b647766748
Rafael Guimarães <rafarodgui@hotmail.com>
2019-10-31 17:07:45 -0300

    Projeto atualizado, incluido funcionalidades de envio de email, recuperação de erros e ambientes de desenvolvimento

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 /* eslint-disable linebreak-style */
+
+import 'dotenv/config';
+
+import * as Sentry from '@sentry/node';
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database';
@@ -9,11 +17,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +35,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'internal server error' });
+    });
   }
 }
 
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'e08db20362135ea4b0c4d6d5ba68a9e8',
+  secret: 'e08db20362135ea4b0c4d6d5ba68a9e8', // process.env.APP_SECRET,
   expiresIn: '7d',
 };
